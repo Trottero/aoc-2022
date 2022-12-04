a318a33a45f853019b820721383266bfd8e65c4a
Trottero <wrlxs@hotmail.com>
2022-12-04 13:16:04 +0100

    Day 4

---
+++ 04/02.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+agg = 0
+
+lines = []
+with open ('./04/input.txt') as f:
+    lines = f.readlines()
+lines = [[tuple([int(sec) for sec in seg.split('-')]) for seg in line.strip().split(',')] for line in lines]
+
+print(lines[0])
+
+def a_contains_b(a, b):
+    a_start, a_end = a
+    b_start, b_end = b
+
+    # Check if a starts in b
+    if a_start <= b_end and a_start >= b_start:
+        return True
+
+    # full overlap
+    return a_start <= b_start and a_end >= b_end
+
+for a, b in lines:
+    if a_contains_b(a, b) or a_contains_b(b, a):
+        agg += 1
+        continue
+
+print(agg)
