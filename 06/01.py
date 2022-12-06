be774adf8d7e4004c8a2117ae67b68b120b1ca3f
Trottero <wrlxs@hotmail.com>
2022-12-06 12:30:33 +0100

    Day 06

---
+++ 06/01.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+agg = 0
+
+marker = []
+with open ('./06/input.txt') as f:
+    line = [line.strip() for line in f.readlines()][0]
+
+marker_len = 4
+for i, c in enumerate(line):
+    marker = line[i:i+marker_len]
+    if len(set(marker)) == len(marker):
+        print(i + marker_len)
+        break 
