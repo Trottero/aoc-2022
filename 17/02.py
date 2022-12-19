1eaaeef87ca37813cafd460c9b6b18ed9eecec24
Trottero <wrlxs@hotmail.com>
2022-12-19 19:34:35 +0100

    Magic code is magic

--- 17/02.py
+++ 17/02.py
@@ -30,12 +30,15 @@ jet_displacement = {
 shape_index = 0
 jet_index = 0
 
-size = 100
+size = 10_000
 
 # Just store it in an array, no one cares its small anyway
 field = np.zeros((size * 3 + 10, 7), dtype=int)
 
 print('created field')
+
+
+history = []
 
 
 def get_highest_true(arr):
@@ -129,15 +132,129 @@ for _ in tqdm.tqdm(range(size)):
     highest_y = get_highest_true(field) + 1
     highest_y_hist.append(highest_y)
 
+    if highest_y > 50:
+        # Append the top 10 rows to the history
+        history.append(field.copy()[highest_y - 50:highest_y])
+    else:
+        history.append(np.zeros((50, 7), dtype=int))
+
 print(highest_y)
+
+
+found = False
+shape_interval = 0
+shape_start = 0
+# Find equals in the history
+for i in range(len(history)):
+    if np.array_equal(history[i], np.zeros((50, 7), dtype=int)):
+        continue
+    for j in range(i + 1, len(history)):
+        if np.array_equal(history[i], history[j]):
+            shape_interval = abs(i - j)
+            found = True
+            print('found equal at: ', i, j)
+            shape_start = i
+            break
+    if found:
+        break
+
+# Deduce the y interval
+y_interval = highest_y_hist[-1] - highest_y_hist[-(shape_interval + 1)]
+print('shape-interval: ', shape_interval, ' y_interval: ', y_interval)
+
+times = int((1000000000000 - shape_start) / shape_interval)
+current_acc = times * y_interval
+sims_left = 1000000000000 - shape_start - (shape_interval * times)
+print('time left: ', sims_left)
+print('current sum: ', current_acc)
 
 
 sns.lineplot(x=range(len(highest_y_hist)), y=highest_y_hist)
 plt.show()
 
-# Compute gradient
-y_diff = highest_y_hist[-1]
-x_diff = len(highest_y_hist)
-grad = y_diff / x_diff
-print(grad)
-print(grad * 1000000000000)
+# Try to find a pattern in highest_y_hist
+
+
+# # Compute gradient
+# y_diff = highest_y_hist[-1]
+# x_diff = len(highest_y_hist)
+# grad = y_diff / x_diff
+# print(grad)
+# print(grad * 1000000000000)
+
+shape_index = 0
+jet_index = 0
+highest_y = 0
+
+size2 = shape_start + sims_left
+
+# current: 1514285711189
+# expected: 1514285714288
+# post-addition: 1514285711295
+
+# Just store it in an array, no one cares its small anyway
+field2 = np.zeros((size * 3 + 10, 7), dtype=int)
+
+for _ in tqdm.tqdm(range(size2)):
+    # Emulate a single piece falling down on the field2
+    piece = shapes[shape_index]
+    piece_width = max([x for x, y in piece])
+    current_shape_x = 2
+    current_shape_y = highest_y + 3
+
+    # print_field2(field2)
+
+    settled = False
+    # Simulate the shape falling down
+    while not settled:
+        # print(current_shape_x, current_shape_y)
+        jet_disp = jet_displacement[lines[jet_index]]
+
+        jet_index += 1
+        jet_index %= len(lines)
+        # Check if the jet displacement is valid with regards to bounds
+        if current_shape_x + jet_disp >= 0 and current_shape_x + jet_disp + piece_width < field2.shape[1]:
+            collision = False
+            # Check if the jet causes any of the blocks to collide with others on the field2
+            for x, y in piece:
+                if field2[current_shape_y + y, current_shape_x + x + jet_disp] == 1:
+                    # collision
+                    collision = True
+                    break
+            if not collision:
+                current_shape_x += jet_disp
+
+        # Check if moving it one down results in an out of bounds
+        if current_shape_y - 1 < 0:
+            settled = True
+            # Place the blocks on the field2
+            for x, y in piece:
+                field2[current_shape_y + y, current_shape_x + x] = 1
+            break
+
+        # Check if the blocks collide with other blocks
+        collision = False
+        for x, y in piece:
+            if field2[current_shape_y + y - 1, current_shape_x + x] == 1:
+                # collision
+                collision = True
+                break
+        if not collision:
+            current_shape_y -= 1
+
+        else:
+            # Place the blocks on the field2
+            for x, y in piece:
+                field2[current_shape_y + y, current_shape_x + x] = 1
+            settled = True
+    # print(current_shape_x, current_shape_y)
+
+    # Increment the shape and jet index
+    shape_index += 1
+    shape_index %= len(shapes)
+    highest_y = get_highest_true(field2) + 1
+
+add_sum = highest_y + current_acc
+print('addition sum: ', add_sum)
+print('expected: ', 1514285714288)
+print('dufference', 1514285714288 - add_sum)
